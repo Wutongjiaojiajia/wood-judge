63c0757486526ba4d37dd5079e0e6bac1db02851
黄家豪 <490899577@qq.com>
2020-06-09 14:34:16 +0800

    更新页面

--- src/api/appenv.js
+++ src/api/appenv.js
@@ -1,5 +1,5 @@
 // 当前程序环境
-// qas(测试) | app(正式)
-const appenv = 'app';
+// dev(测试) | app(正式)
+const appenv = 'dev';
 
 export default appenv;
--- src/api/env.js
+++ src/api/env.js
@@ -1,10 +1,10 @@
 import appenv from './appenv';
 const env={
-    qas:{
+    dev:{
         wood:'http://localhost:3000/',   //木材计算测试地址
     },
     app:{
-        wood:'http://120.24.20.165:3000/',   //木材计算正式地址
+        wood:'http://120.24.20.165:8000/',   //木材计算正式地址
     }
 }
 
